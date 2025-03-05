bd22a897c107ce78073e1a5fe34e48795fe93718
Aayush <theundisputed.ze@gmail.com>
2025-03-05 13:34:07 +0545

    add bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,20 @@
+arr = [5, 4, 3, 2, 1]
+
+# bubble sort takes a total of n-1 passes where,  n= len(arr)
+
+def bubble_sort(arr):
+  is_swapped = False
+  for i in range(len(arr)-1):
+    for j in range(1, len(arr)-i):
+      if arr[j] < arr[j-1]:
+        arr[j], arr[j-1] = arr[j-1], arr[j]
+        is_swapped = True
+  
+    if not is_swapped:
+      break
+  
+
+if __name__ == "__main__":
+  print(f"unsorted_array: {arr}")
+  bubble_sort(arr)
+  print(f"sorted_array: {arr}")
